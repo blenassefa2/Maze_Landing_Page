386ce4dd58348028d07f76e4db516489f3e2eb0e
Blen <62964622+blenassefa2@users.noreply.github.com>
2022-12-02 01:03:27 +0300

    corrected links

--- statics/buttons.js
+++ statics/buttons.js
@@ -2,7 +2,7 @@ const downloadGame = () => {
   var link = document.createElement("a");
   document.body.appendChild(link);
   link.href =
-    "https://github.com/blenassefa2/web_project/archive/refs/heads/master.zip";
+    "https://github.com/blenassefa2/Maze_1/archive/refs/heads/version3.zip";
   link.download = "Medieval_Maze";
   link.click();
 };
